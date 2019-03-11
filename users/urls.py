0829b24f4b59daf86255c832c25b11394afc470e
kaylinkhanal <kyalin.khanal@gmail.com>
2019-03-11 13:54:30 +0545

    done with login and registering the accounts and now we will create assignment tasks

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import DefaultRouter
+from .views import UserViewset
+
+router= DefaultRouter()
+router.register(r'',UserViewset,base_name='users')
+urlpatterns=router.urls
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
+from rest_framework import viewsets
 from django.shortcuts import render
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+#
+class UserViewset(viewsets.ModelViewSet):
+    serializer_class= UserSerializer
+    queryset= User.objects.all()
+#now we can use this userviewset inside our own urls.py
